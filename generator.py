9e5f30f530512c7046ca937bd820f0edd17f9172
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-09 04:03:52 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,6 +5,7 @@ class Generator:
         self.model = AutoModelForCausalLM.from_pretrained(config['generator_model'])
         self.tokenizer = AutoTokenizer.from_pretrained(config['generator_model'])
 
+
     def generate(self, prompt: str) -> str:
         inputs = self.tokenizer(prompt, return_tensors='pt')
         outputs = self.model.generate(**inputs, max_length=100)
