4812cf1bccdb15e57a81fe527bf453626a68a685
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-09 07:03:28 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -10,4 +10,4 @@ class Generator:
         inputs = self.tokenizer(prompt, return_tensors='pt')
         outputs = self.model.generate(**inputs, max_length=100)
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-  
+ 
