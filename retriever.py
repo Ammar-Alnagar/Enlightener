a2fe86f16d474deaae024a5ff5157ef17e1be507
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-09 04:02:28 +0300

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -7,7 +7,7 @@ class Retriever:
         self.index = faiss.IndexFlatL2(embeddings[0].shape[0])
         self.index.add(np.array(embeddings))
 
-    
+     
 
     def retrieve(self, query_embedding: np.ndarray, k: int = 5) -> List[int]:
         distances, indices = self.index.search(query_embedding.reshape(1, -1), k)
