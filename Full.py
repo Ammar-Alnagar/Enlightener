cf9c99572abbc23772bc1f56ed8e41688c9b3a9b
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-12 16:02:12 +0300

    Update Full.py

--- Full.py
+++ Full.py
@@ -13,7 +13,7 @@ class GGUFModel:
         output = self.model(prompt, max_tokens=max_tokens)
         return output['choices'][0]['text']
 
-l
+
 
 # src/utils.py
 def determine_model_type(model_path: str) -> str:
