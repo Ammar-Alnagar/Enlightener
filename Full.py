644b72dbb4d2a06c30375b4a20ac02a1385bb0bd
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-12 16:09:55 +0300

    Update Full.py

--- Full.py
+++ Full.py
@@ -3,7 +3,6 @@ from llama_cpp import Llama
    
 class GGUFModel:
 
-
     def __init__(self, model_path: str, n_ctx: int = 512, n_gpu_layers: int = 0):
         self.model = Llama(model_path=model_path, n_ctx=n_ctx, n_gpu_layers=n_gpu_layers)
 
