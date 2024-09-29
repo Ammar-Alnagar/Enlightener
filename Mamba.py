fe94f4f42258ca7f197ecca7dd5539c488814f99
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-29 11:43:41 +0300

    Update Mamba.py

--- Mamba.py
+++ Mamba.py
@@ -11,7 +11,6 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 
-
 # # Create embeddingsclear
 embeddings = OllamaEmbeddings(model="nomic-embed-text", show_progress=False)
 # embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
